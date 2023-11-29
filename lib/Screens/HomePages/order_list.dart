7c69dfa10fb55a1c00b0fc62c3f2cf10a49451c6
BaberHashmi512 <baberhashmi512@gmail.com>
2023-11-29 19:52:35 +0500

    Order list in Progress

--- lib/Screens/HomePages/order_list.dart
+++ lib/Screens/HomePages/order_list.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:toggle_switch/toggle_switch.dart';
 
@@ -9,38 +10,162 @@ class OrderList extends StatefulWidget {
 }
 
 class _OrderListState extends State<OrderList> {
+  String dropdownValue = "Male";
+  List<Widget> services = <Widget>[
+    Text("Wash"),
+    Text("Iron"),
+    Text("Fold"),
+    Text("Dry"),
+    Text("Clena"),
+  ];
+  late List<bool> _selectedServices = <bool>[true, false, false, false, false];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text("Order List",style: TextStyle(fontWeight: FontWeight.w500,fontSize: 18),),
+        title: const Text(
+          "Order List",
+          style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18),
+        ),
       ),
       body: Column(
         children: [
-          ToggleSwitch(
-            activeBorders: [
-              Border.all(color: Color(0XFF38106A))
-            ],
-            cornerRadius: 12,
-            dividerMargin: 10,
-            dividerColor: Colors.teal,
-            isVertical: false,
-            curve: Curves.bounceIn,
-            minWidth: 70.0,
-            minHeight: 50.0,
-            fontSize: 16.0,
-            initialLabelIndex: 1,
-            activeBgColor: const [Colors.pinkAccent],
-            activeFgColor: Color(0XFF38106A),
-            inactiveBgColor: Colors.transparent,
-            inactiveFgColor: Color(0XFF38106A),
-            totalSwitches: 5,
-            labels: ['Wash', 'Ironing', 'Fold','Dry','Clean'],
-            onToggle: (index) {
-              print('switched to: $index');
-            },
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                ToggleButtons(
+                  renderBorder: false,
+                  fillColor: Colors.white,
+                  onPressed: (val) {
+                    setState(() {
+                      _selectedServices = List.generate(5, (index) => false);
+                      _selectedServices[val] = !_selectedServices[val];
+                    });
+                  },
+                  isSelected: _selectedServices,
+                  children: List<Widget>.generate(
+                    5,
+                    (index) => Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: _selectedServices[index]
+                              ? Colors.pinkAccent
+                              : Colors.white,
+                          border: Border.all(),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        padding: const EdgeInsets.all(5),
+                        width: 100,
+                        height: 30,
+                        alignment: Alignment.center,
+                        child: services[index],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(
+              children: [
+                ListTile(
+                  leading: Image.asset(
+                    "assets/images/tshirt1.png",
+                    width: 46.6,
+                    height: 44,
+                  ),
+                  title: const Text(
+                    "T-Shirt",
+                    style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        color: Color(0XFF38106A)),
+                  ),
+                  subtitle: Row(
+                    children: [
+                      const Text(
+                        "\$2",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            fontSize: 13,
+                            color: Colors.pinkAccent),
+                      ),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      DropdownButton<String>(
+                          value: dropdownValue,
+                          icon: const Icon(Icons.arrow_drop_down_sharp),
+                          style: const TextStyle(color: Colors.black),
+                          items: const [
+                            DropdownMenuItem(
+                              value: "Male",
+                              child: Text("Male"),
+                            ),
+                            DropdownMenuItem(
+                              value: "Female",
+                              child: Text("Female"),
+                            ),
+                            DropdownMenuItem(
+                              value: "Gender",
+                              child: Text("Gender"),
+                            ),
+                          ],
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              dropdownValue = newValue!;
+                            });
+                          })
+                    ],
+                  ),
+                  trailing: Wrap(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: const Color(0XFFC3C8D2),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: const Icon(
+                          CupertinoIcons.minus,
+                          color: Color(0XFF38106A),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      const Text(
+                        "4",
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w500),
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: const Color(0XFFC3C8D2),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: const Icon(
+                          Icons.add,
+                          color: Color(0XFF38106A),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
